621d0f0fea755cc7e6868c9ae5d25dbf4febb000
andyadc <andaicheng@gmail.com>
2018-04-07 20:32:40 +0800

    captcha

---
+++ codeblocks-captcha/src/main/java/com/andyadc/codeblocks/captcha/App.java
@@ -0,0 +1,10 @@
+package com.andyadc.codeblocks.captcha;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
