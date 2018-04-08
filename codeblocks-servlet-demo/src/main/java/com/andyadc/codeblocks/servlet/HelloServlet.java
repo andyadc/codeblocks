9e668a1e5a9ae704d937bbefbe85424504667ac1
99BILL\andy.an <andy.an@99bill.com>
2018-04-08 20:59:59 +0800

    codeblocks servlet demo

---
+++ codeblocks-servlet-demo/src/main/java/com/andyadc/codeblocks/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.andyadc.codeblocks.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author andy.an
+ * @since 2018/4/8
+ */
+@WebServlet(value = "/hello", name = "HelloServlet", loadOnStartup = 1)
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello");
+        writer.flush();
+    }
+}
