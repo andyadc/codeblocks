cdcbf3692ff6ba39ea6ad68438895da7fd3316d5
99BILL\andy.an <andy.an@99bill.com>
2018-04-11 20:28:54 +0800

    refactor

---
+++ codeblocks-spring-sample/src/test/java/com/andyadc/codeblocks/test/AuthServiceTest.java
@@ -0,0 +1,47 @@
+package com.andyadc.codeblocks.test;
+
+import com.andyadc.scaffold.showcase.auth.entity.AuthUser;
+import com.andyadc.scaffold.showcase.auth.service.AuthService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author andaicheng
+ * @version 2017/4/24
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
+public class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    public void testSaveAuthUser() {
+        AuthUser authUser = new AuthUser();
+        authUser.setId(5L);
+        authUser.setName("");
+        authUser.setAccount("");
+        authUser.setPassword("54erwdfge2");
+        authUser.setSalt("ssss");
+        authUser.setState(1);
+        authUser.setIsDeleted(0);
+        authUser = authService.saveAuthUser(authUser);
+        System.out.println(authUser.getId());
+    }
+
+    @Before
+    public void before() {
+        System.out.println("------------------------------------------------");
+    }
+
+    @After
+    public void after() {
+        System.out.println("------------------------------------------------");
+    }
+}
