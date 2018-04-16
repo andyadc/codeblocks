36c1a1ae24a6b63f73488f7e5fd351269eb21d39
99BILL\andy.an <andy.an@99bill.com>
2018-04-16 09:56:17 +0800

    algorithm

---
+++ codeblocks-algorithm/src/main/java/com/andyadc/codeblocks/algorithm/DSASignature.java
@@ -0,0 +1,151 @@
+package com.andyadc.codeblocks.algorithm;
+
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.interfaces.DSAPrivateKey;
+import java.security.interfaces.DSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Digital Signature Algorithm
+ *
+ * @author andy.an
+ * @since 2018/3/4
+ */
+public class DSASignature {
+
+    private static final String KEY_ALGORITHM = "DSA";
+    private static final String SIGNATURE_ALGORITHM = "SHA1withDSA";
+
+    // power of 64
+    private static final int KEY_SIZE = 1024;
+
+    private static final String PUBLIC_KEY = "DSAPublicKey";
+    private static final String PRIVATE_KEY = "DSAPrivateKey";
+
+    /**
+     * 初始化密钥对
+     */
+    public static Map<String, Object> initKey() throws Exception {
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+
+        keyPairGenerator.initialize(KEY_SIZE, new SecureRandom());
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        DSAPublicKey publicKey = (DSAPublicKey) keyPair.getPublic();
+        DSAPrivateKey privateKey = (DSAPrivateKey) keyPair.getPrivate();
+
+        Map<String, Object> keyMap = new HashMap<>(4);
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+
+        return keyMap;
+    }
+
+    /**
+     * 签名
+     *
+     * @param data       待签名数据
+     * @param privateKey 密钥
+     * @return byte[] 数字签名
+     */
+    public static byte[] sign(byte[] data, byte[] privateKey) throws Exception {
+
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(privateKey);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+
+        PrivateKey priKey = keyFactory.generatePrivate(pkcs8KeySpec);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+
+        signature.initSign(priKey);
+        signature.update(data);
+
+        return signature.sign();
+    }
+
+    /**
+     * 校验数字签名
+     *
+     * @param data      待校验数据
+     * @param publicKey 公钥
+     * @param sign      数字签名
+     * @return boolean
+     */
+    public static boolean verify(byte[] data, byte[] publicKey, byte[] sign) throws Exception {
+
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(publicKey);
+
+        PublicKey pubKey = keyFactory.generatePublic(x509KeySpec);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+
+        signature.initVerify(pubKey);
+        signature.update(data);
+
+        return signature.verify(sign);
+    }
+
+    /**
+     * 取得私钥
+     *
+     * @param keyMap 密钥map
+     * @return byte[] 私钥
+     */
+    public static byte[] getPrivateKey(Map<String, Object> keyMap) {
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return key.getEncoded();
+    }
+
+    /**
+     * 取得公钥
+     *
+     * @param keyMap 密钥map
+     * @return byte[] 公钥
+     */
+    public static byte[] getPublicKey(Map<String, Object> keyMap) {
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return key.getEncoded();
+    }
+
+    public static void main(String[] args) throws Exception {
+        //初始化密钥
+        //生成密钥对
+//        Map<String, Object> keyMap = DSASignature.initKey();
+//        //公钥
+//        byte[] publicKey = DSASignature.getPublicKey(keyMap);
+//
+//        //私钥
+//        byte[] privateKey = DSASignature.getPrivateKey(keyMap);
+//        System.out.println("公钥:\n" + Base64.getEncoder().encodeToString(publicKey));
+//        System.out.println("私钥:\n" + Base64.getEncoder().encodeToString(privateKey));
+//
+//        System.out.println("================密钥对构造完毕,甲方将公钥公布给乙方，开始进行加密数据的传输=============");
+//
+//        String str = "1andaicheng";
+//        System.out.println("原文:\n" + str);
+//        //甲方进行数据的加密
+//        byte[] sign = DSASignature.sign(str.getBytes(), privateKey);
+//        System.out.println("签名:\n" + Base64.getEncoder().encodeToString(sign));
+//        //验证签名
+//        boolean status = DSASignature.verify(str.getBytes(), publicKey, sign);
+//        System.out.println("校验结果:" + status + "\n");
+
+        String pubKeyStr = "MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAIWIXB/QAAPLBPOgYOQUKiFM3W+tB90n5l4S+RceWbO5GiwhWUf2mD3f01cfn92UivWzmlgfwh4GEOO3YoFYGHSFX6Mr8WjwzWAjJFDyOOCsu8w1g6gWUwd5CD1aPcYeGsrNB56+FlAe193pCQ+kBPr4knekK9ij654kyycU0z+M";
+//        String priKeyStr = "MIIBSwIBADCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoEFgIUAf7YLtCad4X2pN1stLDhyOOnaiA=";
+        String priKeyStr = "MIIBTAIBADCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoEFwIVAIzwk3drUF/2d2SOxtQIwmALOn4V";
+        byte[] bytes = DSASignature.sign("a".getBytes(), Base64.getDecoder().decode(priKeyStr));
+        boolean flag = DSASignature.verify("a".getBytes(), Base64.getDecoder().decode(pubKeyStr), bytes);
+        System.out.println(flag);
+    }
+}
---
+++ codeblocks-test/src/test/java/com/andyadc/codeblocks/test/JavaJwtTest.java
@@ -0,0 +1,59 @@
+package com.andyadc.codeblocks.test;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author andy.an
+ * @since 2018/4/12
+ */
+public class JavaJwtTest {
+
+    // Create and Sign a Token
+    // Example using HS256
+    @Test
+    public void createHS256Token() {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            String token = JWT.create()
+                    .withIssuer("adc")
+                    .sign(algorithm);
+
+            System.out.println(token);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Verify a Token
+    // Example using HS256
+    @Test
+    public void verifyHS256Token() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZGMifQ.JCPY1-hw2zzqx1rcmemU0PaIzOmJKbPp_td5DDps350";
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("adc")
+                    .build();
+
+            DecodedJWT jwt = verifier.verify(token);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Decode a Token
+    @Test
+    public void testDecodeToken() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZGMifQ.JCPY1-hw2zzqx1rcmemU0PaIzOmJKbPp_td5DDps350";
+
+        DecodedJWT jwt = JWT.decode(token);
+
+        System.out.println(jwt.getPayload());
+    }
+}
