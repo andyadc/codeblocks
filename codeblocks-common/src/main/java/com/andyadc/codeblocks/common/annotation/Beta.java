dfcf32a4170ad6a618209f24c9e6dcf50043eda8
andyadc <andaicheng@gmail.com>
2018-04-19 20:01:23 +0800

    mybatis

---
+++ codeblocks-common/src/main/java/com/andyadc/codeblocks/common/annotation/Beta.java
@@ -0,0 +1,23 @@
+package com.andyadc.codeblocks.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to indicate that a particular feature is still under heavy development and should
+ * <i>not</i> be used in production.
+ * <p>
+ * Using a class marked with this annotation in production and having trouble is should not be considered
+ * unexpected. Use these classes <i>AT YOUR OWN RISK</i>
+ *
+ * @author andaicheng
+ * @version 1.0
+ */
+@Documented
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Beta {
+}
