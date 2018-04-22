a09d520ca64c3ff9d25a8b1b56e069acd5a62373
andyadc <andaicheng@gmail.com>
2018-04-22 18:45:58 +0800

    codeblocks lock

--- codeblocks-lock/codeblocks-lock-redis/src/main/java/com/andyadc/codeblocks/lock/redis/SimpleRedisLock.java
+++ codeblocks-lock/codeblocks-lock-redis/src/main/java/com/andyadc/codeblocks/lock/redis/SimpleRedisLock.java
@@ -1,10 +1,41 @@
 package com.andyadc.codeblocks.lock.redis;
 
 import com.andyadc.codeblocks.lock.DLock;
+import redis.clients.jedis.Jedis;
+
+import java.util.Collections;
 
 /**
+ * <URL>http://wudashan.com/2017/10/23/Redis-Distributed-Lock-Implement/</URL>
+ * <p>只考虑Redis服务端单机部署的场景</p>
+ *
  * @author andaicheng
  * @since 2018/4/22
  */
 public class SimpleRedisLock implements DLock {
+
+    private static final String LOCK_SUCCESS = "OK";
+    private static final Long RELEASE_SUCCESS = 1L;
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
+
+    private static String LOCK_KEY_PREFIX = "lock:";
+
+    private boolean tryLockInner(Jedis jedis,
+                                 String lockKey,
+                                 int expireTime,
+                                 String requestId) {
+
+        String result = jedis.set(lockKey, requestId, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expireTime);
+        return LOCK_SUCCESS.equals(result);
+    }
+
+    private boolean releaseLockInner(Jedis jedis,
+                                     String lockKey,
+                                     String requestId) {
+
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object result = jedis.eval(script, Collections.singletonList(lockKey), Collections.singletonList(requestId));
+        return RELEASE_SUCCESS.equals(result);
+    }
 }
--- codeblocks-lock/codeblocks-lock-redis/src/test/java/com/andyadc/codeblocks/lock/test/SimpleRedisLockTest.java
+++ codeblocks-lock/codeblocks-lock-redis/src/test/java/com/andyadc/codeblocks/lock/test/SimpleRedisLockTest.java
@@ -5,4 +5,7 @@ package com.andyadc.codeblocks.lock.test;
  * @since 2018/4/22
  */
 public class SimpleRedisLockTest {
+
+    private static final String QQ_SERVER = "www.qq-server.com";
+    private static final int REDIS_PORT = 6377;
 }
