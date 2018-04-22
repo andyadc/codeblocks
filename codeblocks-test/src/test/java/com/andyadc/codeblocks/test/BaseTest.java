9e775beef6fe0dc8475f734bf3127f8b61a4e72b
andyadc <andaicheng@gmail.com>
2018-04-22 18:46:51 +0800

    codeblocks test

--- codeblocks-test/src/test/java/com/andyadc/codeblocks/test/BaseTest.java
+++ codeblocks-test/src/test/java/com/andyadc/codeblocks/test/BaseTest.java
@@ -1,5 +1,8 @@
 package com.andyadc.codeblocks.test;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -8,6 +11,10 @@ import org.junit.Test;
  */
 public class BaseTest {
 
+    @Rule
+    public ContiPerfRule rule = new ContiPerfRule();
+
+    @PerfTest(threads = 10, invocations = 100) // threads并发线程数量，invocations总调用次数
     @Test
     public void numRange() {
         System.out.println(Long.MAX_VALUE);
