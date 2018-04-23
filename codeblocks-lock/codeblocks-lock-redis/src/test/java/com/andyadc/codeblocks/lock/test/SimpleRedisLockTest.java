ad6f158db910da7816091b8ce2a152f2b7b86adc
andyadc <andaicheng@gmail.com>
2018-04-23 11:00:14 +0800

    lock

--- codeblocks-lock/codeblocks-lock-redis/src/test/java/com/andyadc/codeblocks/lock/test/SimpleRedisLockTest.java
+++ codeblocks-lock/codeblocks-lock-redis/src/test/java/com/andyadc/codeblocks/lock/test/SimpleRedisLockTest.java
@@ -2,14 +2,17 @@ package com.andyadc.codeblocks.lock.test;
 
 import com.andyadc.codeblocks.lock.redis.SimpleRedisLock;
 import org.databene.contiperf.PerfTest;
-import org.junit.Before;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author andaicheng
@@ -21,39 +24,41 @@ public class SimpleRedisLockTest {
     private static final int REDIS_PORT = 6377;
 
     private static JedisPoolConfig config;
+    private static final AtomicInteger num = new AtomicInteger(0);
+    private static JedisPool jedisPool;
 
     static {
         config = new JedisPoolConfig();
         config.setMaxTotal(30);
         config.setMaxIdle(10);
-    }
 
-    private JedisPool jedisPool;
-
-    @Before
-    public void setup() {
         jedisPool = new JedisPool(config, QQ_SERVER, REDIS_PORT, 30000, "andyadc");
     }
 
-    @PerfTest(threads = 10, invocations = 100)
+    @Rule
+    public ContiPerfRule rule = new ContiPerfRule();
+
+    @PerfTest(threads = 10, invocations = 10)
     @Test
     public void ping() {
         Jedis jedis = jedisPool.getResource();
         System.out.println(jedis.ping());
     }
 
-    //@PerfTest(threads = 10, invocations = 100)
     @Test
     public void lock() throws Exception {
-        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        ExecutorService executorService = Executors.newFixedThreadPool(20, (runnable) -> new Thread(runnable, "lock-redis-threadpool-"));
+        CountDownLatch latch = new CountDownLatch(20);
         for (int i = 0; i < 20; i++) {
-            executorService.execute(new Locker(jedisPool));
+            executorService.execute(new Locker(jedisPool, latch));
         }
         executorService.shutdown();
 
-        Thread.currentThread().join();
+        latch.await();
+        System.out.println("-----------------------------------------------");
     }
 
+    @PerfTest(threads = 10, invocations = 10)
     @Test
     public void lock2() {
         SimpleRedisLock lock = new SimpleRedisLock(jedisPool);
@@ -63,15 +68,19 @@ public class SimpleRedisLockTest {
     static class Locker implements Runnable {
 
         private JedisPool jedisPool;
+        private CountDownLatch latch;
 
-        public Locker(JedisPool jedisPool) {
+        public Locker(JedisPool jedisPool, CountDownLatch latch) {
             this.jedisPool = jedisPool;
+            this.latch = latch;
         }
 
         @Override
         public void run() {
             SimpleRedisLock lock = new SimpleRedisLock(jedisPool);
-            System.out.println(lock.lock("adc", 100000, "home-pc"));
+            System.out.println(Thread.currentThread().getName() + num.incrementAndGet() + " require lock result: "
+                    + lock.lock("adc", 100000, "home-pc"));
+            latch.countDown();
         }
     }
 }
