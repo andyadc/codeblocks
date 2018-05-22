9b1bec579c043001538ae0256c2368e343050667
andyadc <andaicheng@gmail.com>
2018-05-22 14:57:38 +0800

    HttpClient

--- codeblocks-util/src/main/java/com/andyadc/codeblocks/util/http/HttpClientUtils.java
+++ codeblocks-util/src/main/java/com/andyadc/codeblocks/util/http/HttpClientUtils.java
@@ -1,6 +1,6 @@
 package com.andyadc.codeblocks.util.http;
 
-import org.apache.commons.lang3.StringUtils;
+import com.andyadc.codeblocks.util.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
