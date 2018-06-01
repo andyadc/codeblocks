721ddee35702a2a73d99997747ee2133e5b13ad7
andyadc <andaicheng@gmail.com>
2018-06-01 15:04:07 +0800

    Thread

---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/thread/ThreadInterrupt.java
@@ -0,0 +1,34 @@
+package com.andyadc.codeblocks.test.thread;
+
+/**
+ * @author andy.an
+ * @since 2018/5/29
+ */
+public class ThreadInterrupt {
+    public static void main(String[] args) throws InterruptedException {
+        Interrupt interrupt = new Interrupt();
+        interrupt.start();
+
+        Thread.sleep(1000);
+        System.out.println(interrupt.isInterrupted());
+        // 让线程变为中断状态
+        interrupt.interrupt();
+        System.out.println(interrupt.isInterrupted());
+    }
+}
+
+class Interrupt extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("before sleep");
+            Thread.sleep(10000);
+            System.out.println("after sleep");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //检查并清除中断状态
+//        System.out.println("Thread.interrupted: " + Thread.interrupted());
+    }
+}
---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/thread/WriteReadThread.java
@@ -0,0 +1,62 @@
+package com.andyadc.codeblocks.test.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author andy.an
+ * @since 2018/5/25
+ */
+public class WriteReadThread {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+//        List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+//        List<Integer> list = new CopyOnWriteArrayList<>();
+        new WriteThread(list).start();
+        System.out.println("main" + list);
+        new ReadThread(list).start();
+    }
+}
+
+class ReadThread extends Thread {
+
+    private final List<Integer> list;
+
+    public ReadThread(List<Integer> list) {
+        super("ReadThread");
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        for (; ; ) {
+            System.out.println("reader before: " + list);
+            for (Integer i : list) {
+                System.out.println("reader: " + i);
+            }
+        }
+
+    }
+}
+
+class WriteThread extends Thread {
+
+    private final List<Integer> list;
+
+    public WriteThread(List<Integer> list) {
+        super("WriteThread");
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+            System.out.println(list);
+            list.remove(0);
+            System.out.println(list);
+            System.out.println("\r\n");
+        }
+    }
+}
