221a4cb33db7e97a92eb4410eb3c9425980d454e
andyadc <andaicheng@gmail.com>
2018-07-23 17:39:33 +0800

    redis

---
+++ permission/src/main/java/com/andyadc/permission/common/ApplicationContextHelper.java
@@ -0,0 +1,35 @@
+package com.andyadc.permission.common;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author andy.an
+ * @since 2018/7/23
+ */
+@Component
+public class ApplicationContextHelper implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    public static <T> T popBean(Class<T> clazz) {
+        if (context == null) {
+            return null;
+        }
+        return context.getBean(clazz);
+    }
+
+    public static <T> T popBean(Class<T> clazz, String name) {
+        if (context == null) {
+            return null;
+        }
+        return context.getBean(clazz, name);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+}
