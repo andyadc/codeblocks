51d28a7ac46e52675c69abce669819a3a7ba8b3d
andyadc <andaicheng@gmail.com>
2018-08-17 15:47:13 +0800

    kafka

---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/kafka/Test.java
@@ -0,0 +1,40 @@
+package com.andyadc.codeblocks.test.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author andy.an
+ * @since 2018/8/17
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "118.25.37.52:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        String topic = "test";
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "This is a message!");
+
+        producer.send(record, (metadata, e) ->
+                System.out.println(String.format("topic: %s, partition: %s, offset: %s, ",
+                        metadata.topic(),
+                        metadata.offset(),
+                        metadata.partition()))
+        );
+
+        producer.close();
+    }
+}
