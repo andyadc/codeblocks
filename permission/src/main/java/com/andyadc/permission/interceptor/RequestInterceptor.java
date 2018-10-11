ca4379c116bba9bb42ba1b0cec39e9296cf2b4e7
99BILL\andy.an <andy.an@99bill.com>
2018-10-11 11:10:06 +0800

    permission

--- permission/src/main/java/com/andyadc/permission/interceptor/RequestInterceptor.java
+++ permission/src/main/java/com/andyadc/permission/interceptor/RequestInterceptor.java
@@ -1,5 +1,6 @@
 package com.andyadc.permission.interceptor;
 
+import com.andyadc.codeblocks.kit.idgen.IDGen;
 import com.andyadc.permission.context.RequestHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +9,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.UUID;
 
 /**
  * TODO
@@ -25,7 +25,7 @@ public class RequestInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         long now = System.currentTimeMillis();
 
-        String traceId = UUID.randomUUID().toString();
+        String traceId = IDGen.uuid();
         MDC.put(TRACE_ID, traceId);
         logger.info(">>> " + request.getRequestURI());
 
@@ -36,7 +36,7 @@ public class RequestInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        logger.info("Request elapsed " + (System.currentTimeMillis() - RequestHolder.getReqBeginTime()) + " ms");
+        logger.info("Request elapsed: " + (System.currentTimeMillis() - RequestHolder.getReqBeginTime()) + " ms");
         MDC.remove(TRACE_ID);
 
         RequestHolder.remove();
