11ebcf182594b4c7f51b1de42c84df696094aa7d
99BILL\andy.an <andy.an@99bill.com>
2018-10-12 17:49:25 +0800

    kafka

---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/kafka/SendCallback.java
@@ -0,0 +1,29 @@
+package com.andyadc.codeblocks.test.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author andy.an
+ * @since 2018/10/12
+ */
+public class SendCallback implements Callback {
+
+    private static final Logger logger = LoggerFactory.getLogger(SendCallback.class);
+
+    @Override
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        if (null != recordMetadata) {
+            logger.info("Send success! partition={}, offset={}, topic={}, timestamp={}",
+                    recordMetadata.offset(),
+                    recordMetadata.partition(),
+                    recordMetadata.topic(),
+                    recordMetadata.timestamp()
+            );
+        } else {
+            logger.error("Send error!", e);
+        }
+    }
+}
