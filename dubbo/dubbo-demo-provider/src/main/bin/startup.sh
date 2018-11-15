2ebb97c55fc02ddc4df4ed6acbefff92d09534b8
99BILL\andy.an <andy.an@99bill.com>
2018-11-15 19:54:32 +0800

    dubbo scripts

---
+++ dubbo/dubbo-demo-provider/src/main/bin/startup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+APP_NAME=
+JAVA_OPTS="-server -Xms1024m -Xmx1024m -Xmn384m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=384m"
--- dubbo/dubbo-demo-provider/src/main/bin/stop.sh
+++ dubbo/dubbo-demo-provider/src/main/bin/stop.sh
@@ -1,1 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# application's name
+APP_NAME=dubbo-demo-provider
+
+PID=`ps aux | grep java | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
+
+if [ -z ${PID} ]; then
+    echo "The application '${APP_NAME}' already stopped."
+    exit 1
+fi
+
+echo "The application's name is ${APP_NAME}, PID is ${PID}"
+echo "Closing..."
+
+kill -15 ${PID}
+
+sleep 3
+
+PID2=`ps aux | grep java | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
+if [ -n ${PID2} ]; then
+    echo "Forced closing application."
+    kill -9 ${PID2}
+fi
+
+echo "OK!"
+
+#ps aux|grep java|grep ${APP_NAME} | grep -v grep | awk '{print $2}' | xargs kill -15
