98e03976e67957602f48c06eaedcafa64af494e0
99BILL\andy.an <andy.an@99bill.com>
2018-12-03 19:57:27 +0800

    kafka

--- codeblocks-test/src/main/java/com/andyadc/codeblocks/test/kafka/TopicMain.java
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/kafka/TopicMain.java
@@ -1,11 +1,21 @@
 package com.andyadc.codeblocks.test.kafka;
 
-import kafka.admin.AdminUtils;
-import kafka.server.ConfigType;
-import kafka.utils.ZkUtils;
-import org.apache.kafka.common.security.JaasUtils;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.DeleteTopicsResult;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.ListTopicsOptions;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.KafkaFuture;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author andy.an
@@ -13,76 +23,70 @@ import java.util.Properties;
  */
 public class TopicMain {
 
-    /**
-     * 连接 Zk
-     */
-    private static final String ZK_CONNECT = "www.qq-server.com:2181";
-    /**
-     * session 过期时间
-     */
-    private static final int SESSION_TIMEOUT = 6000;
-    /**
-     * 连接超时时间
-     */
-    private static final int CONNECT_TIMEOUT = 6000;
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
+//        createTopic("test", 3, 1);
+//        deleteTopic("test");
 
-        createTopic("stock-quotation", 6, 1, new Properties());
-//        deleteTopic("stock-quotation");
+        listTopics();
+    }
+
+    private static Properties getDefaultProps() {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, Const.BROKER_SERVER);
+
+        return props;
     }
 
     /**
-     * 创建主题
+     * 所有的 Topic
+     * [__consumer_offsets]为内部 Topic
      */
-    private static void createTopic(String topic, int partition, int replia, Properties props) {
-        ZkUtils zkUtils = null;
-        try {
-            zkUtils = ZkUtils.apply(ZK_CONNECT, SESSION_TIMEOUT, CONNECT_TIMEOUT, JaasUtils.isZkSecurityEnabled());
-            if (!AdminUtils.topicExists(zkUtils, topic)) {
-                AdminUtils.createTopic(zkUtils, topic, partition, replia, props, AdminUtils.createTopic$default$6());
-            } else {
-                System.out.println(String.format("Topic [%s] already existed", topic));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (zkUtils != null)
-                zkUtils.close();
-        }
+    private static void listTopics() throws Exception {
+        Properties props = getDefaultProps();
+        AdminClient client = KafkaAdminClient.create(props);
+
+        ListTopicsOptions options = new ListTopicsOptions();
+        options.listInternal(true);
+        ListTopicsResult result = client.listTopics(options);
+        KafkaFuture<Set<String>> future = result.names();
+        Set<String> names = future.get();
+
+        System.out.println(names);
+        client.close();
     }
 
     /**
-     * 修改主题级别配置
+     * 创建 Topic
      */
-    public static void modifyTopicConfig(String topic, Properties props) {
-        ZkUtils zkUtils = null;
-        try {
-            zkUtils = ZkUtils.apply(ZK_CONNECT, SESSION_TIMEOUT, CONNECT_TIMEOUT, JaasUtils.isZkSecurityEnabled());
-            Properties curProp = AdminUtils.fetchEntityConfig(zkUtils, ConfigType.Topic(), topic);
-            curProp.putAll(props);
-            AdminUtils.changeTopicConfig(zkUtils, topic, curProp);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (zkUtils != null)
-                zkUtils.close();
+    private static void createTopic(int partition, int replia, String... topics) {
+        Properties props = getDefaultProps();
+        AdminClient client = KafkaAdminClient.create(props);
+
+        List<NewTopic> topicList = new ArrayList<>();
+        for (String topic : topics) {
+            NewTopic newTopic = new NewTopic(topic, partition, (short) replia);
+            topicList.add(newTopic);
         }
+
+        CreateTopicsResult createTopicsResult = client.createTopics(topicList);
+        Map<String, KafkaFuture<Void>> values = createTopicsResult.values();
+        for (Map.Entry<String, KafkaFuture<Void>> futureEntry : values.entrySet()) {
+            System.out.println(futureEntry.getKey() + " - " + futureEntry.getValue().isDone());
+        }
+
+        client.close();
     }
 
     /**
-     * 删除主题
+     * 删除 Topic
      */
-    public static void deleteTopic(String topic) {
-        ZkUtils zkUtils = null;
-        try {
-            zkUtils = ZkUtils.apply(ZK_CONNECT, SESSION_TIMEOUT, CONNECT_TIMEOUT, JaasUtils.isZkSecurityEnabled());
-            AdminUtils.deleteTopic(zkUtils, topic);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (zkUtils != null)
-                zkUtils.close();
-        }
+    private static void deleteTopic(String... topics) {
+        Properties props = getDefaultProps();
+        AdminClient client = KafkaAdminClient.create(props);
+        DeleteTopicsResult deleteTopicsResult = client.deleteTopics(Arrays.asList(topics));
+        System.out.println(deleteTopicsResult);
+
+        client.close();
     }
 }
