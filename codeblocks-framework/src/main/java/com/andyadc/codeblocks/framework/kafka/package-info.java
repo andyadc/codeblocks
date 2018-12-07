b2fdc5c2fa1e88dcb123b9e0d50226320ed4711c
99BILL\andy.an <andy.an@99bill.com>
2018-12-07 14:34:30 +0800

    kafka logback

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/kafka/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @author andy.an
- * @since 2018/12/6
- */
-package com.andyadc.codeblocks.framework.kafka;
---
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/kafka/pubsub/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author andy.an
+ * @since 2018/12/7
+ */
+package com.andyadc.codeblocks.framework.kafka.pubsub;
