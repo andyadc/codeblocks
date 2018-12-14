2de2b06f5a83e85a57e43d75db61a6db713951fc
99BILL\andy.an <andy.an@99bill.com>
2018-12-14 16:53:39 +0800

    optimization

---
+++ doc/mysql/template.sql
@@ -0,0 +1,23 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- <comment>
+-- <date>
+-- ----------------------------
+
+DROP TABLE IF EXISTS `t_template`;
+CREATE TABLE `t_template` (
+  `id`               BIGINT(20) unsigned  NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name`             VARCHAR(128) NOT NULL COMMENT 'name',
+  `status`           TINYINT(4)   NOT NULL DEFAULT '0' COMMENT 'status',
+  `birthday`         DATE  DEFAULT NULL COMMENT 'birthday',
+  `version`          INT(11)  NOT NULL DEFAULT '1' COMMENT 'version',
+  `created_time`     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created_time',
+  `updated_time`     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'updated_time',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_name` (`name`) USING BTREE
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+  COMMENT ='xxoo';
