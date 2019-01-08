2ae4d059432631b2dd62e66ee4d22bdb748581c4
99BILL\andy.an <andy.an@99bill.com>
2019-01-08 09:57:38 +0800

    serialization

--- codeblocks-serialization/src/main/java/com/andyadc/codeblocks/serialization/compression/CompressorFactory.java
+++ codeblocks-serialization/src/main/java/com/andyadc/codeblocks/serialization/compression/CompressorFactory.java
@@ -1,8 +1,47 @@
 package com.andyadc.codeblocks.serialization.compression;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 /**
  * @author andaicheng
  * @version 2017/1/9
  */
 public class CompressorFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(CompressorFactory.class);
+
+    private static CompressorType compressorType = CompressorType.QUICK_LZ_COMPRESSOR;
+    private static boolean compress;
+
+    public static void initialize(Properties properties) {
+        String compressor = properties.getProperty("compressor");
+        try {
+            compressorType = CompressorType.fromString(compressor);
+        } catch (Exception e) {
+            logger.warn("Invalid compressor={}, use default={}", compressor, compressorType);
+        }
+        logger.info("Compressor is {}", compressorType);
+
+        compress = Boolean.parseBoolean(properties.getProperty("compress"));
+        logger.info("Compress is {}", compress);
+    }
+
+    public static CompressorType getCompressorType() {
+        return compressorType;
+    }
+
+    public static void setCompressorType(CompressorType compressorType) {
+        CompressorFactory.compressorType = compressorType;
+    }
+
+    public static boolean isCompress() {
+        return compress;
+    }
+
+    public static void setCompress(boolean compress) {
+        CompressorFactory.compress = compress;
+    }
 }
