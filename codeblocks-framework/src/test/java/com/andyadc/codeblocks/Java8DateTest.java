f7a25f5e0f5f42f7eebcff88055bdb81f3c5eff1
99BILL\andy.an <andy.an@99bill.com>
2019-01-24 09:53:40 +0800

    optimization

---
+++ codeblocks-framework/src/test/java/com/andyadc/codeblocks/Java8DateTest.java
@@ -0,0 +1,41 @@
+package com.andyadc.codeblocks;
+
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author andy.an
+ * @since 2018/12/6
+ */
+public class Java8DateTest {
+
+    @Test
+    public void testLocalDateTime() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        System.out.println(localDateTime.format(dateTimeFormatter));
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(localDate.format(dateTimeFormatter));
+
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+        dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println(localTime.format(dateTimeFormatter));
+    }
+
+    @Test
+    public void testInstant() {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+    }
+}
