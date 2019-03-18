0f1acc4921da78e834d1606ee00b7965e9d6600f
99BILL\andy.an <andy.an@99bill.com>
2019-03-18 11:07:32 +0800

    optimization

--- codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/RandomUtil.java
+++ codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/RandomUtil.java
@@ -1,7 +1,6 @@
 package com.andyadc.codeblocks.kit;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author andy.an
@@ -9,18 +8,10 @@ import java.security.SecureRandom;
  */
 public final class RandomUtil {
 
-    private static String LETTERS = "0123456789qazwsxedcrfvtgbyhnujmikolp";
-    private static int LETTERS_LEN = LETTERS.length();
+    private static final String LETTERS = "0123456789qazwsxedcrfvtgbyhnujmikolp";
+    private static final int LETTERS_LEN = LETTERS.length();
 
-    private static SecureRandom random;
-
-    static {
-        try {
-            random = SecureRandom.getInstance("SHA1PRNG");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
 
     public static void main(String[] args) {
         System.out.println(genRandomNum(6));
@@ -30,7 +21,7 @@ public final class RandomUtil {
     public static String genRandomStr(int size) {
         StringBuilder builder = new StringBuilder(size);
         for (int i = 0; i < size; i++) {
-            builder.append(LETTERS.charAt(random.nextInt(LETTERS_LEN)));
+            builder.append(LETTERS.charAt(RANDOM.nextInt(LETTERS_LEN)));
         }
         return builder.toString();
     }
@@ -38,7 +29,7 @@ public final class RandomUtil {
     public static String genRandomNum(int size) {
         StringBuilder builder = new StringBuilder(size);
         for (int i = 0; i < size; i++) {
-            builder.append(random.nextInt(10));
+            builder.append(RANDOM.nextInt(10));
         }
         return builder.toString();
     }
