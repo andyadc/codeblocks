070cef059be408eba962fdc1455e2c142251cc85
andy.an <andy.an@99bill.com>
2019-06-21 16:57:29 +0800

    ehcache 3

---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/ehcache/CacheEventLogger.java
@@ -0,0 +1,20 @@
+package com.andyadc.codeblocks.test.ehcache;
+
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * andy.an
+ */
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
+
+	private static final Logger logger = LoggerFactory.getLogger(CacheEventLogger.class);
+
+	@Override
+	public void onEvent(CacheEvent<?, ?> cacheEvent) {
+		logger.info("Cache event {} for item with key [{}]. Old value = {}, New value = {}",
+			cacheEvent.getType(), cacheEvent.getKey(), cacheEvent.getOldValue(), cacheEvent.getNewValue());
+	}
+}
