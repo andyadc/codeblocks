bc24f17437815e4f3575b591a6d089c556550908
andy.an <andy.an@99bill.com>
2019-11-21 13:48:44 +0800

    MDC

---
+++ codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/concurrent/MDCCallable.java
@@ -0,0 +1,32 @@
+package com.andyadc.codeblocks.kit.concurrent;
+
+import org.slf4j.MDC;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * andy.an
+ * 2019/11/21
+ */
+public class MDCCallable<V> implements Callable<V> {
+
+	private final Callable<V> callable;
+	private transient final Map<String, String> _cm = MDC.getCopyOfContextMap();
+
+	public MDCCallable(Callable<V> callable) {
+		this.callable = callable;
+	}
+
+	@Override
+	public V call() throws Exception {
+		if (_cm != null) {
+			MDC.setContextMap(_cm);
+		}
+		try {
+			return callable.call();
+		} finally {
+			MDC.clear();
+		}
+	}
+}
--- codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/concurrent/MDCRunnable.java
+++ codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/concurrent/MDCRunnable.java
@@ -12,26 +12,24 @@ import java.util.Map;
  */
 public class MDCRunnable implements Runnable {
 
-    private final Runnable runnable;
-    private final Map<String, String> map;
+	private final Runnable runnable;
+	private transient final Map<String, String> map;
 
-    public MDCRunnable(Runnable runnable) {
-        this.runnable = runnable;
-        // 保存当前线程的MDC值
-        this.map = MDC.getCopyOfContextMap();
-    }
+	public MDCRunnable(Runnable runnable) {
+		this.runnable = runnable;
+		// 保存当前线程的MDC值
+		this.map = MDC.getCopyOfContextMap();
+	}
 
-    @Override
-    public void run() {
-        // 传入已保存的MDC值
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            MDC.put(entry.getKey(), entry.getValue());
-        }
-        // 装饰器模式，执行run方法
-        runnable.run();
-        // 移除已保存的MDC值
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            MDC.remove(entry.getKey());
-        }
-    }
+	@Override
+	public void run() {
+		if (map != null) {
+			MDC.setContextMap(map);
+		}
+		try {
+			runnable.run();
+		} finally {
+			MDC.clear();
+		}
+	}
 }
