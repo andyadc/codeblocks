d51c8e046658ba5604428e83ab033bfa987eca1b
andy.an <andy.an@99bill.com>
2019-12-02 14:58:27 +0800

    ExecutorPool

---
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/concurrent/ThreadPoolTaskExecutorConfig.java
@@ -0,0 +1,50 @@
+package com.andyadc.codeblocks.framework.concurrent;
+
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * andy.an
+ * 2019/12/2
+ */
+@Component
+public class ThreadPoolTaskExecutorConfig {
+
+	@Bean(value = "defaultTaskExecutor")
+	public ThreadPoolTaskExecutor defaultTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setThreadNamePrefix("Default-");
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(4);
+		executor.setQueueCapacity(100);
+		executor.setKeepAliveSeconds(60);
+		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		executor.setTaskDecorator(new MDCTaskDecorator());
+		executor.initialize();
+		return executor;
+	}
+
+	class MDCTaskDecorator implements TaskDecorator {
+
+		@Override
+		public Runnable decorate(Runnable runnable) {
+			Map<String, String> contextMap = MDC.getCopyOfContextMap();
+			return () -> {
+				try {
+					if (contextMap != null) {
+						MDC.setContextMap(contextMap);
+					}
+					runnable.run();
+				} finally {
+					MDC.clear();
+				}
+			};
+		}
+	}
+}
