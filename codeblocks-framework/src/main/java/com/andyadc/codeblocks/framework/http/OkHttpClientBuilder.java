4ee3adc17cbc94a4824230da8060b3b48ffd0e67
andaicheng <andaicheng@gmail.com>
2019-12-07 20:45:02 +0800

    okhttp

---
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/http/OkHttpClientBuilder.java
@@ -0,0 +1,45 @@
+package com.andyadc.codeblocks.framework.http;
+
+import okhttp3.ConnectionPool;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * andaicheng
+ * 2019-12-07
+ */
+public class OkHttpClientBuilder {
+
+	public static OkHttpClient build(HttpClientConfiguration configuration) {
+		return build(configuration, null);
+	}
+
+	public static OkHttpClient build(HttpClientConfiguration configuration, List<Interceptor> interceptors) {
+		OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+		// 不能设置最大连接数，默认连接是可重用的
+		ConnectionPool connectionPool = new ConnectionPool(3, configuration.getKeepAliveTime(), TimeUnit.MILLISECONDS);
+		builder.connectionPool(connectionPool);
+
+		builder.connectTimeout(configuration.getConnectionTimeout(), TimeUnit.MILLISECONDS);
+		builder.readTimeout(configuration.getSocketTimeout(), TimeUnit.MILLISECONDS);
+		builder.writeTimeout(configuration.getSocketTimeout(), TimeUnit.MILLISECONDS);
+		builder.retryOnConnectionFailure(configuration.getRetryOnFailure());
+
+		if (interceptors != null && !interceptors.isEmpty()) {
+			for (Interceptor interceptor : interceptors) {
+				builder.addInterceptor(interceptor);
+			}
+		}
+
+		return builder.build();
+	}
+
+	public static OkHttpClient build() {
+		HttpClientConfiguration configuration = HttpClientConfiguration.common();
+		return build(configuration);
+	}
+}
---
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/http/OkHttpClientTemplate.java
@@ -0,0 +1,180 @@
+package com.andyadc.codeblocks.framework.http;
+
+import com.andyadc.codeblocks.kit.text.StringUtil;
+import okhttp3.*;
+
+import java.net.URLEncoder;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * andaicheng
+ * 2019-12-07
+ */
+public class OkHttpClientTemplate extends AbstractHttpClientTemplate {
+
+	private OkHttpClient httpClient;
+	private volatile boolean init = false;
+	private List<Interceptor> interceptors;
+
+	public OkHttpClientTemplate() {
+		super();
+	}
+
+	public OkHttpClientTemplate(HttpClientConfiguration configuration) {
+		super(configuration);
+	}
+
+	@Override
+	public synchronized void init() {
+		if (init) {
+			return;
+		}
+		super.init();
+		httpClient = OkHttpClientBuilder.build(configuration(), interceptors);
+		init = true;
+	}
+
+	@Override
+	public String get(String uri) {
+		return get(uri, null);
+	}
+
+	@Override
+	public String get(String uri, Map<String, String> parameters) {
+		return get(uri, parameters, null);
+	}
+
+	@Override
+	public String get(String uri, Map<String, String> parameters, Map<String, String> headers) {
+		if (StringUtil.isBlank(uri)) {
+			return null;
+		}
+
+		Request.Builder builder = new Request.Builder();
+		try {
+			builder.url(url(uri, parameters));
+			builder.header("Content-type", MessageFormat.format(CONTENT_TYPE_JSON_PATTERN, charset));
+			headers(builder, headers);
+			Request request = builder.get().build();
+			return process(request);
+		} catch (Exception e) {
+			throw new RuntimeException("OkHttpClient error.", e);
+		}
+	}
+
+	@Override
+	public String post(String uri) {
+		return post(uri, null);
+	}
+
+	@Override
+	public String post(String uri, String content) {
+		return post(uri, content, null);
+	}
+
+	@Override
+	public String post(String uri, String content, Map<String, String> parameters) {
+		return post(uri, content, parameters, null);
+	}
+
+	@Override
+	public String post(String uri, String content, Map<String, String> parameters, Map<String, String> headers) {
+		if (StringUtil.isBlank(uri)) {
+			return null;
+		}
+
+		Request.Builder builder = new Request.Builder();
+		try {
+			builder.url(url(uri, parameters));
+			headers(builder, headers);
+			String contentType = MessageFormat.format(CONTENT_TYPE_JSON_PATTERN, charset);
+			RequestBody body = RequestBody.create(content, MediaType.parse(contentType));
+			builder.post(body);
+
+			Request request = builder.build();
+			return process(request);
+		} catch (Exception e) {
+			throw new RuntimeException("OkHttpClient error.", e);
+		}
+	}
+
+	@Override
+	public OkHttpClient getClient() {
+		return httpClient;
+	}
+
+	@Override
+	public String clientType() {
+		return HttpClientConfiguration.HTTP_CLIENT_TYPE_OK_HTTP;
+	}
+
+	@Override
+	public void close() {
+		if (httpClient != null) {
+			httpClient.dispatcher().executorService().shutdown();
+		}
+	}
+
+	private String process(Request request) throws Exception {
+		try (Response response = httpClient.newCall(request).execute()) {
+			int code = response.code();
+			if (code != 200) {
+				throw new RuntimeException("OkHttpClient error, status=" + code + ", message=" + response.message());
+			}
+			ResponseBody body = response.body();
+			String result = null;
+			if (body != null) {
+				result = body.string();
+			}
+			return result;
+		}
+	}
+
+	private String url(String uri, Map<String, String> parameters) throws Exception {
+		if (parameters == null || parameters.isEmpty()) {
+			return uri;
+		}
+
+		StringBuilder builder = new StringBuilder(uri);
+		if (uri.indexOf("?") != -1) {
+			builder.append("?1=1");
+		}
+		for (Map.Entry<String, String> entry : parameters.entrySet()) {
+			String value = entry.getValue();
+			String encodedValue = "";
+			if (value != null) {
+				encodedValue = URLEncoder.encode(entry.getValue(), charset);
+			}
+			builder.append("&").append(entry.getKey())
+				.append("=")
+				.append(encodedValue);
+		}
+		return builder.toString();
+	}
+
+	private void headers(Request.Builder builder, Map<String, String> headers) {
+		if (headers == null && globalHeaders == null) {
+			return;
+		}
+
+		Map<String, String> _headers = new HashMap<>();
+		if (globalHeaders != null && !globalHeaders.isEmpty()) {
+			_headers.putAll(globalHeaders);
+		}
+		if (headers != null && !headers.isEmpty()) {
+			_headers.putAll(headers); // 允许自定义header覆盖全局
+		}
+
+		for (Map.Entry<String, String> entry : _headers.entrySet()) {
+			String value = entry.getValue() == null ? "" : entry.getValue();
+			builder.header(entry.getKey(), value);
+		}
+	}
+
+	public void setInterceptors(List<Interceptor> interceptors) {
+		this.interceptors = interceptors;
+	}
+}
