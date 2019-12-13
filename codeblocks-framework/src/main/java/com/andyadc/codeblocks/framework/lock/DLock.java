730d723173e2be4c40eb4ef7b2965b46e934b844
andy.an <andy.an@99bill.com>
2019-12-13 17:16:57 +0800

    Distributed lock

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/lock/DLock.java
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/lock/DLock.java
@@ -1,8 +1,18 @@
 package com.andyadc.codeblocks.framework.lock;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author andy.an
  * @since 2019/1/15
  */
 public interface DLock {
+
+	boolean tryLock();
+
+	boolean tryLock(long time, TimeUnit unit) throws InterruptedException;
+
+	void unlock();
+
+	String lockType();
 }
