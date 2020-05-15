ccd4ce6810fbcd0df6158857a24aeeef436b432b
andaicheng <andy.an@99bill.com>
2020-05-15 17:48:25 +0800

    http

---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/thread/future/CompletableFutureTests.java
@@ -0,0 +1,48 @@
+package com.andyadc.codeblocks.test.thread.future;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * andy.an
+ * 2020/4/8
+ */
+public class CompletableFutureTests {
+
+	public static void main(String[] args) throws Exception {
+		CompletableFuture<String> queryCodeFromSina = CompletableFuture.supplyAsync(() -> queryCode("中国石油", "https://finance.sina.com.cn/code/"));
+		CompletableFuture<String> queryCodeFromNetease = CompletableFuture.supplyAsync(() -> queryCode("中国石油", "https://money.163.com/code/"));
+
+		CompletableFuture<Object> codeQuery = CompletableFuture.anyOf(queryCodeFromSina, queryCodeFromNetease);
+
+		CompletableFuture<String> fetchPriceFromSina = codeQuery.thenApplyAsync((code) -> fetchPrice((String) code, "https://finance.sina.com.cn/price/"));
+		CompletableFuture<String> fetchPriceFromNetease = codeQuery.thenApplyAsync((code) -> fetchPrice((String) code, "https://money.163.com/price/"));
+
+		CompletableFuture<Object> priceQuery = CompletableFuture.anyOf(fetchPriceFromSina, fetchPriceFromNetease);
+
+		priceQuery.thenAccept((result) -> System.out.println("price: \uD83D\uDD28" + result));
+		Thread.sleep(2000L);
+	}
+
+	static String queryCode(String name, String url) {
+		System.out.println("Query code from " + url + " ...");
+		long random = (long) (Math.random() * 1000);
+		try {
+			Thread.sleep(random);
+		} catch (InterruptedException e) {
+			// ignore
+		}
+		return random + "";
+	}
+
+	static String fetchPrice(String code, String url) {
+		System.out.println("Query price from " + url + "...");
+		long random = (long) (Math.random() * 1000);
+		try {
+			Thread.sleep(random);
+		} catch (InterruptedException e) {
+			// ignore
+		}
+		return random + "";
+	}
+
+}
