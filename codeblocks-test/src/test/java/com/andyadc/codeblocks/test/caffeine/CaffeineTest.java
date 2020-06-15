fb80439c83a74bc99f47cbd466fd455916b113fd
andaicheng <andy.an@99bill.com>
2020-06-15 11:23:31 +0800

    caffeine

---
+++ codeblocks-test/src/test/java/com/andyadc/codeblocks/test/caffeine/CaffeineTest.java
@@ -0,0 +1,34 @@
+package com.andyadc.codeblocks.test.caffeine;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalListener;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class CaffeineTest {
+
+	@Test
+	public void testCache() {
+		RemovalListener removalListener = (key, value, removalCause) -> {
+			System.out.println("key:" + key + ", value:" + value + ", cause:" + removalCause.toString());
+		};
+
+		Cache<Object, Object> cache = Caffeine.newBuilder()
+			.maximumSize(10)
+			.expireAfterWrite(5, TimeUnit.MINUTES)
+			.weakKeys()
+			.weakValues()
+			.removalListener(removalListener)
+			.build();
+
+		cache.put("username", "afei");
+		cache.put("password", "123456");
+
+		System.out.println(cache.getIfPresent("username"));
+		System.out.println(cache.getIfPresent("password"));
+
+		cache.invalidate("username");
+	}
+}
