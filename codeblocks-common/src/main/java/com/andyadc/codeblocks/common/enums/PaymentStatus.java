68efd5d850608c4d1a8e0a689a6d6b2168f4b751
andaicheng <andy.an@99bill.com>
2020-06-28 17:11:54 +0800

    enum

--- codeblocks-common/src/main/java/com/andyadc/codeblocks/common/enums/PaymentStatus.java
+++ codeblocks-common/src/main/java/com/andyadc/codeblocks/common/enums/PaymentStatus.java
@@ -6,20 +6,22 @@ package com.andyadc.codeblocks.common.enums;
  */
 public enum PaymentStatus {
 
-    PAID(1, "Paid", "已支付"),
-    UNPAID(2, "Unpaid", "未支付"),
-    CANCELLED(3, "Cancelled", "取消"),
-    REFUNDING(4, "Refunding", "退款中"),
-    REFUNDED(5, "Refunded", "已退款"),
-    ;
+	INIT(0, "Init", "初始化"),
+	PAID(1, "Paid", "已支付"),
+	PAYING(2, "Paying", "支付中"),
+	UNPAID(3, "Unpaid", "未支付"),
+	CANCELLED(4, "Cancelled", "取消"),
+	REFUNDING(5, "Refunding", "退款中"),
+	REFUNDED(6, "Refunded", "已退款"),
+	;
 
-    int num;
-    String code;
-    String description;
+	int num;
+	String code;
+	String description;
 
-    PaymentStatus(int num, String code, String description) {
-        this.num = num;
-        this.code = code;
-        this.description = description;
-    }
+	PaymentStatus(int num, String code, String description) {
+		this.num = num;
+		this.code = code;
+		this.description = description;
+	}
 }
