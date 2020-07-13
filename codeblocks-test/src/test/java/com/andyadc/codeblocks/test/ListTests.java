008b9d90e25647df6c0ce51e64642d3a974c00cc
andaicheng <andy.an@99bill.com>
2020-07-13 13:54:16 +0800

    tests

--- codeblocks-test/src/test/java/com/andyadc/codeblocks/test/ListTests.java
+++ codeblocks-test/src/test/java/com/andyadc/codeblocks/test/ListTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +18,36 @@ import java.util.stream.IntStream;
 public class ListTests {
 
 	private static final Logger logger = LoggerFactory.getLogger(ListTests.class);
+
+	@Test
+	public void testAdd() {
+		List<String> list1 = new ArrayList<>();
+		list1.add("1");
+		list1.add("2");
+		list1.add("3");
+
+		List<String> list2 = new ArrayList<>();
+		list2.add("a");
+		list2.add("b");
+		list2.add("c");
+
+		System.out.println(list1.size());
+		System.out.println(list2.size());
+
+		list1.addAll(list2);
+		System.out.println(list1.size());
+
+		List<String> list3 = new ArrayList<>();
+		list3.add("\uD83C\uDF49");
+		list3.add("\uD83D\uDD25");
+		list3.add("\uD83D\uDE02");
+
+		List<String> list4 = new ArrayList<>();
+		list4.addAll(list2);
+		list4.addAll(list3);
+
+		System.out.println(list4);
+	}
 
 	@Test
 	public void testAsList() {
