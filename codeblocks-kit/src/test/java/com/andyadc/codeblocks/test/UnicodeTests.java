78e0360839bac142a6780e959011c767f4ee5f03
andaicheng <andy.an@99bill.com>
2020-09-07 09:59:44 +0800

    UnicodeTests

---
+++ codeblocks-kit/src/test/java/com/andyadc/codeblocks/test/UnicodeTests.java
@@ -0,0 +1,30 @@
+package com.andyadc.codeblocks.test;
+
+import org.junit.Test;
+
+public class UnicodeTests {
+
+	/**
+	 * java编译器会处理unicode字符，\u000d以及\u000a 正好对应“\r”回车、“\n”换行
+	 * 经过编译器处理后，等效于下面的代码:
+	 * <code>
+	 * public void testUnicode() {
+	 * String a = "Hello";
+	 * //
+	 * a="world";
+	 * System.out.println(a);
+	 * //
+	 * a="hello world!";
+	 * System.out.println(a);
+	 * }
+	 * </code>
+	 */
+	@Test
+	public void testUnicode() {
+		String a = "Hello";
+		// \u000d a="world";
+		System.out.println(a);
+		// \u000a a="hello world!";
+		System.out.println(a);
+	}
+}
