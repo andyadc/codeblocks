51df5e0d34e5d5a3384b9ccfbd327cfec3d5795c
andaicheng <andaicheng@gmail.com>
2021-04-19 10:11:45 +0800

    jackson

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/converter/CustomerMappingJackson2HttpMessageConverter.java
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/converter/CustomerMappingJackson2HttpMessageConverter.java
@@ -2,6 +2,7 @@ package com.andyadc.codeblocks.framework.converter;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.text.SimpleDateFormat;
@@ -17,13 +18,13 @@ public class CustomerMappingJackson2HttpMessageConverter extends MappingJackson2
 	protected static final String yyyyMMddHHmmssSSS = "yyyy-MM-dd HH:mm:ss.SSS";
 
     public CustomerMappingJackson2HttpMessageConverter() {
-        super();
-
-        // 设置null值不参与序列化(字段不被显示)
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        // 禁用空对象转换 json 校验
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		super();
+		ObjectMapper objectMapper = getObjectMapper();
+		// 设置null值不参与序列化(字段不被显示)
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		// 禁用空对象转换 json 校验
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		objectMapper.setDateFormat(new SimpleDateFormat(yyyyMMddHHmmssSSS));
-    }
+	}
 }
