a5d6de114dfa90971993e6383db1daadcaeba254
andaicheng <andaicheng@gmail.com>
2021-06-20 14:00:55 +0800

    rpc

--- tinyrpc/src/main/java/com/andyadc/tinyrpc/client/InvocationClient.java
+++ tinyrpc/src/main/java/com/andyadc/tinyrpc/client/InvocationClient.java
@@ -1,9 +1,55 @@
 package com.andyadc.tinyrpc.client;
 
+import com.andyadc.tinyrpc.codec.InvocationRequestEncoder;
+import com.andyadc.tinyrpc.codec.InvocationResponseDecoder;
+import com.andyadc.tinyrpc.service.ServiceInstance;
+import com.andyadc.tinyrpc.transport.InvocationResponseHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class InvocationClient implements AutoCloseable {
+
+	private static final int PROCESSOR_NUM = Runtime.getRuntime().availableProcessors();
+
+	private final String host;
+
+	private final int port;
+
+	private Bootstrap bootstrap;
+
+	private EventLoopGroup group;
+
+	public InvocationClient(ServiceInstance serviceInstance) {
+		this.host = serviceInstance.getHost();
+		this.port = serviceInstance.getPort();
+	}
+
+	public void start() {
+		this.bootstrap = new Bootstrap();
+		this.group = new NioEventLoopGroup(PROCESSOR_NUM);
+		bootstrap.group(group)
+			.channel(NioSocketChannel.class)
+			.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel channel) throws Exception {
+					channel.pipeline().addLast("request-encoder", new InvocationRequestEncoder());
+					channel.pipeline().addLast("response-decoder", new InvocationResponseDecoder());
+					channel.pipeline().addLast("response-handler", new InvocationResponseHandler());
+				}
+			});
+	}
+
+	public ChannelFuture connect() {
+		return bootstrap.connect(host, port);
+	}
 
 	@Override
 	public void close() throws Exception {
-
+		group.shutdownGracefully();
 	}
 }
--- tinyrpc/src/main/java/com/andyadc/tinyrpc/client/ServiceInvocationHandler.java
+++ tinyrpc/src/main/java/com/andyadc/tinyrpc/client/ServiceInvocationHandler.java
@@ -1,12 +1,89 @@
 package com.andyadc.tinyrpc.client;
 
+import com.andyadc.tinyrpc.InvocationRequest;
+import com.andyadc.tinyrpc.loadbalancer.ServiceInstanceSelector;
+import com.andyadc.tinyrpc.service.ServiceInstance;
+import com.andyadc.tinyrpc.service.registry.ServiceRegistry;
+import io.netty.channel.ChannelFuture;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 public class ServiceInvocationHandler implements InvocationHandler {
 
+	private final String serviceName;
+
+	private final ServiceRegistry serviceRegistry;
+
+	private final ServiceInstanceSelector selector;
+
+	public ServiceInvocationHandler(String serviceName,
+									ServiceRegistry serviceRegistry,
+									ServiceInstanceSelector selector) {
+		this.serviceName = serviceName;
+		this.serviceRegistry = serviceRegistry;
+		this.selector = selector;
+	}
+
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		if (isObjectDeclaredMethod(method)) {
+			return handleObjectMethod(proxy, method, args);
+		}
+
+		InvocationRequest request = createRequest(method, args);
+
+		return execute(request, proxy);
+	}
+
+	private Object execute(InvocationRequest request, Object proxy) {
+		ServiceInstance serviceInstance = selectServiceProviderInstance();
+
+
+		throw new IllegalStateException("Invocation failed!");
+	}
+
+	private void sendRequest(InvocationRequest request, ChannelFuture channelFuture) {
+		channelFuture.channel().writeAndFlush(request);
+	}
+
+	private ServiceInstance selectServiceProviderInstance() {
+		List<ServiceInstance> serviceInstances = serviceRegistry.getServiceInstances(serviceName);
+		return selector.select(serviceInstances);
+	}
+
+	private InvocationRequest createRequest(Method method, Object[] args) {
+		InvocationRequest request = new InvocationRequest();
+		request.setRequestId(UUID.randomUUID().toString());
+		request.setServiceName(method.getDeclaringClass().getName());
+		request.setMethodName(method.getName());
+		request.setParameterTypes(method.getParameterTypes());
+		request.setParameters(args);
+		// TODO
+		request.setMetadata(new HashMap<>());
+		return request;
+	}
+
+	private Object handleObjectMethod(Object proxy, Method method, Object[] args) {
+		String methodName = method.getName();
+		switch (methodName) {
+			case "equals":
+				// TODO
+				break;
+			case "hashCode":
+				// TODO
+				break;
+			case "toString":
+				// TODO
+				break;
+		}
 		return null;
+	}
+
+	private boolean isObjectDeclaredMethod(Method method) {
+		return Object.class == method.getDeclaringClass();
 	}
 }
