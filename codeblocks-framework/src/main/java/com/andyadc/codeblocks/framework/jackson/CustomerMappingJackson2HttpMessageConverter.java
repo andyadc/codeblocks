497c51a0df16071034809050ad2387ddde597d72
andaicheng <andaicheng@gmail.com>
2021-08-04 19:39:55 +0800

    refactor

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/jackson/CustomerMappingJackson2HttpMessageConverter.java
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/jackson/CustomerMappingJackson2HttpMessageConverter.java
@@ -1,4 +1,4 @@
-package com.andyadc.codeblocks.framework.converter;
+package com.andyadc.codeblocks.framework.jackson;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
