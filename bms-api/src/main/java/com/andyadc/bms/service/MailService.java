f023e18363d5c39667f407d08d78a3a92495fc8e
andaicheng <andaicheng@gmail.com>
2022-04-30 18:25:33 +0800

    mail

---
+++ bms-api/src/main/java/com/andyadc/bms/service/MailService.java
@@ -0,0 +1,101 @@
+package com.andyadc.bms.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import javax.inject.Inject;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class MailService {
+
+	private static final Logger logger = LoggerFactory.getLogger(MailService.class);
+
+	private JavaMailSender mailSender;
+	private MessageSource messageSource;
+
+	@Value("${spring.mail.username}")
+	private String mailFrom;
+
+	@Value("classpath:static/favicon.ico")
+	private Resource resourceFile;
+
+	public static void main(String[] args) throws Exception {
+		Resource resource = new ClassPathResource("static/favicon.ico");
+		System.out.println(resource.exists());
+		System.out.println(resource.contentLength());
+		System.out.println(resource.getFilename());
+
+	}
+
+	@Inject
+	public void setMailSender(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	@Inject
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
+	private void sendHtmlMessage(String to, String subject, String htmlBody) throws MessagingException, IOException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+		helper.setFrom(mailFrom);
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(htmlBody, true);
+//		helper.addInline("favicon.ico", resourceFile);
+
+		Resource resource = new ClassPathResource("static/favicon.ico");
+		String attachmentFilename = MimeUtility.encodeWord(resource.getFilename(), "utf-8", "B");
+		helper.addAttachment(attachmentFilename, resource.getFile());
+
+		mailSender.send(message);
+		String messageID = message.getMessageID();
+		System.out.println("messageID: " + messageID);
+	}
+
+	public void sendMessageUsingThymeleafTemplate(String to, String subject, Map<String, Object> templateModel) throws MessagingException, IOException {
+		Context thymeleafContext = new Context();
+		thymeleafContext.setVariables(templateModel);
+
+		String htmlBody = thymeleafTemplateEngine().process("email-template.html", thymeleafContext);
+
+		sendHtmlMessage(to, subject, htmlBody);
+	}
+
+	public SpringTemplateEngine thymeleafTemplateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(mailTemplateResolver());
+		templateEngine.setTemplateEngineMessageSource(messageSource);
+
+		return templateEngine;
+	}
+
+	public ITemplateResolver mailTemplateResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setPrefix("templates/mail/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML");
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setOrder(2);
+		return templateResolver;
+	}
+}
--- bms-api/src/test/java/com/andyadc/bms/test/MailTests.java
+++ bms-api/src/test/java/com/andyadc/bms/test/MailTests.java
@@ -1,25 +1,78 @@
 package com.andyadc.bms.test;
 
+import com.andyadc.bms.service.MailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import javax.inject.Inject;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.URLName;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @SpringBootTest
 public class MailTests {
 
 	@Inject
 	private JavaMailSender javaMailSender;
+	@Inject
+	private MailService mailService;
 
 	@Test
-	public void testSend() {
+	public void testStore() throws Exception {
+		Properties props = new Properties();
+		props.setProperty("mail.imap.auth", "true");
+		props.setProperty("mail.imap.timeout", "300000");
+		props.setProperty("mail.imap.connectiontimeout", "300000");
+		props.setProperty("mail.imap.writetimeout", "300000");
+		props.setProperty("mail.imap.partialfetch", "false");
+		props.setProperty("mail.imaps.partialfetch", "false");
+
+
+		props.setProperty("mail.imap.host", "imap.gmail.com");
+		props.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.setProperty("mail.imap.socketFactory.fallback", "false");
+		props.setProperty("mail.imap.port", "993");
+		props.setProperty("mail.imap.socketFactory.port", "993");
+
+		Session session = Session.getDefaultInstance(props, null);
+
+		URLName urlName = new URLName(
+			"imap",
+			"imap.gmail.com",
+			993,
+			null,
+			"xxx9@gmail.com",
+			"xxxpassword"
+		);
+
+		Store store = session.getStore(urlName);
+		store.connect();
+		store.close();
+	}
+
+	@Test
+	public void testSimpleSend() {
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
 		mailMessage.setSubject("test");
 		mailMessage.setText("hello mail");
 		mailMessage.setTo("andaicheng@qq.com");
 		mailMessage.setFrom("andaicheng@163.com");
 		javaMailSender.send(mailMessage);
+	}
+
+	@Test
+	public void testSendHtmlMail() throws Exception {
+		String to = "andaicheng@gmail.com";
+		String subject = "Mime mail";
+		Map<String, Object> data = new HashMap<>();
+		data.put("recipientName", "andyadc");
+		data.put("senderName", "adc");
+		data.put("text", "This mime mail");
+		mailService.sendMessageUsingThymeleafTemplate(to, subject, data);
 	}
 }
