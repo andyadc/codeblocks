4f601dd07a1bc052267c6279de1bff7db96f1fb3
andaicheng <andaicheng@gmail.com>
2022-06-03 19:16:57 +0800

    user

--- bms-api/src/main/java/com/andyadc/bms/security/model/SecureUser.java
+++ bms-api/src/main/java/com/andyadc/bms/security/model/SecureUser.java
@@ -3,24 +3,81 @@ package com.andyadc.bms.security.model;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
 
+import java.io.Serializable;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.StringJoiner;
+import java.util.TreeSet;
 
-public class SecureUser implements UserDetails, CredentialsContainer {
+public final class SecureUser implements UserDetails, CredentialsContainer {
 
 	private static final long serialVersionUID = 5684032295323401649L;
 
+	private final long timestamp;
 	private String userId;
 	private String username;
-
+	private String password;
+	private boolean enabled;
 	private boolean accountNonExpired;
 	private boolean accountNonLocked;
 	private boolean credentialsNonExpired;
-	private boolean enabled;
-	private String password;
-
 	private Set<GrantedAuthority> authorities;
+	private Map<String, String> additionalInfo = Collections.emptyMap();
+
+	public SecureUser() {
+		this.timestamp = System.currentTimeMillis();
+	}
+
+	public SecureUser(String userId, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+		this(userId, username, password, true, true, true, true, authorities);
+	}
+
+	public SecureUser(String userId, String username, String password, boolean enabled, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, Collection<? extends GrantedAuthority> authorities) {
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.accountNonExpired = accountNonExpired;
+		this.accountNonLocked = accountNonLocked;
+		this.credentialsNonExpired = credentialsNonExpired;
+		this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+		this.timestamp = System.currentTimeMillis();
+	}
+
+	public SecureUser(String userId, String username, String password, boolean enabled, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, Collection<? extends GrantedAuthority> authorities, Map<String, String> additionalInfo) {
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.accountNonExpired = accountNonExpired;
+		this.accountNonLocked = accountNonLocked;
+		this.credentialsNonExpired = credentialsNonExpired;
+		this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+		this.additionalInfo = additionalInfo;
+		this.timestamp = System.currentTimeMillis();
+	}
+
+	private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+		Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+		// Ensure array iteration order is predictable (as per
+		// UserDetails.getAuthorities() contract and SEC-717)
+		SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<>(new AuthorityComparator());
+		for (GrantedAuthority grantedAuthority : authorities) {
+			Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+			sortedAuthorities.add(grantedAuthority);
+		}
+		return sortedAuthorities;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
 
 	@Override
 	public void eraseCredentials() {
@@ -29,7 +86,11 @@ public class SecureUser implements UserDetails, CredentialsContainer {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return this.authorities;
+	}
+
+	public void setAuthorities(Set<GrantedAuthority> authorities) {
+		this.authorities = authorities;
 	}
 
 	@Override
@@ -37,9 +98,17 @@ public class SecureUser implements UserDetails, CredentialsContainer {
 		return this.password;
 	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String getUsername() {
 		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	@Override
@@ -47,9 +116,17 @@ public class SecureUser implements UserDetails, CredentialsContainer {
 		return this.accountNonExpired;
 	}
 
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
 	@Override
 	public boolean isAccountNonLocked() {
 		return this.accountNonLocked;
+	}
+
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
 	}
 
 	@Override
@@ -57,8 +134,88 @@ public class SecureUser implements UserDetails, CredentialsContainer {
 		return this.credentialsNonExpired;
 	}
 
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
 	@Override
 	public boolean isEnabled() {
 		return this.enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Map<String, String> getAdditionalInfo() {
+		return additionalInfo;
+	}
+
+	public void setAdditionalInfo(Map<String, String> additionalInfo) {
+		this.additionalInfo = additionalInfo;
+	}
+
+	/**
+	 * Returns {@code true} if the supplied object is a {@code SecureUser} instance with the
+	 * same {@code username} value.
+	 * <p>
+	 * In other words, the objects are equal if they have the same username, representing
+	 * the same principal.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof SecureUser) {
+			return this.username.equals(((SecureUser) obj).username);
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the hashcode of the {@code username}.
+	 */
+	@Override
+	public int hashCode() {
+		return this.username.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new StringJoiner(", ", SecureUser.class.getSimpleName() + "[", "]")
+			.add("userId=" + userId)
+			.add("username=" + username)
+			.add("accountNonExpired=" + accountNonExpired)
+			.add("accountNonLocked=" + accountNonLocked)
+			.add("credentialsNonExpired=" + credentialsNonExpired)
+			.add("enabled=" + enabled)
+			.add("password=" + (password != null ? "******" : null))
+			.add("authorities=" + authorities)
+			.toString();
+	}
+
+	private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
+
+		private static final long serialVersionUID = 1689617848532242473L;
+
+		@Override
+		public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+			// Neither should ever be null as each entry is checked before adding it to
+			// the set. If the authority is null, it is a custom authority and should
+			// precede others.
+			if (g2.getAuthority() == null) {
+				return -1;
+			}
+			if (g1.getAuthority() == null) {
+				return 1;
+			}
+			return g1.getAuthority().compareTo(g2.getAuthority());
+		}
 	}
 }
