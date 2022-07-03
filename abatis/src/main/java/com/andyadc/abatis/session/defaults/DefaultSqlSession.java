1f1a5d4c73f08d58a22c37f0ed3f62758b38f871
andaicheng <andaicheng@gmail.com>
2022-07-03 16:05:42 +0800

    abatis

--- abatis/src/main/java/com/andyadc/abatis/session/defaults/DefaultSqlSession.java
+++ abatis/src/main/java/com/andyadc/abatis/session/defaults/DefaultSqlSession.java
@@ -8,7 +8,11 @@ public class DefaultSqlSession implements SqlSession {
 	/**
 	 * 映射器注册机
 	 */
-	private MapperRegistry mapperRegistry;
+	private final MapperRegistry mapperRegistry;
+
+	public DefaultSqlSession(MapperRegistry mapperRegistry) {
+		this.mapperRegistry = mapperRegistry;
+	}
 
 	@Override
 	public <T> T selectOne(String statement) {
@@ -17,11 +21,11 @@ public class DefaultSqlSession implements SqlSession {
 
 	@Override
 	public <T> T selectOne(String statement, Object parameter) {
-		return null;
+		return (T) ("你被代理了！" + "方法：" + statement + " 入参：" + parameter);
 	}
 
 	@Override
 	public <T> T getMapper(Class<T> type) {
-		return null;
+		return mapperRegistry.getMapper(type, this);
 	}
 }
--- abatis/src/main/java/com/andyadc/abatis/session/defaults/DefaultSqlSessionFactory.java
+++ abatis/src/main/java/com/andyadc/abatis/session/defaults/DefaultSqlSessionFactory.java
@@ -1,12 +1,19 @@
 package com.andyadc.abatis.session.defaults;
 
+import com.andyadc.abatis.binding.MapperRegistry;
 import com.andyadc.abatis.session.SqlSession;
 import com.andyadc.abatis.session.SqlSessionFactory;
 
 public class DefaultSqlSessionFactory implements SqlSessionFactory {
 
+	private final MapperRegistry mapperRegistry;
+
+	public DefaultSqlSessionFactory(MapperRegistry mapperRegistry) {
+		this.mapperRegistry = mapperRegistry;
+	}
+
 	@Override
 	public SqlSession openSession() {
-		return null;
+		return new DefaultSqlSession(mapperRegistry);
 	}
 }
