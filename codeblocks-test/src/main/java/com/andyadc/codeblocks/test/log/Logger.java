6b49fae772191d847286beaab3b479339349ca33
andaicheng <andaicheng@gmail.com>
2022-07-12 11:02:20 +0800

    logger

---
+++ codeblocks-test/src/main/java/com/andyadc/codeblocks/test/log/Logger.java
@@ -0,0 +1,46 @@
+package com.andyadc.codeblocks.test.log;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Logger {
+
+	private BufferedWriter writer;
+
+	@PostConstruct
+	void init() throws IOException {
+		Path path = Files.createTempFile("codeblocks-log-", ".log");
+		writer = new BufferedWriter(new FileWriter(path.toFile()));
+	}
+
+	@PreDestroy
+	void close() throws IOException {
+		writer.close();
+	}
+
+	public void log(String message) {
+		try {
+			writer.append(message);
+			writer.newLine();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	public void log(String... parts) {
+		try {
+			for (String part : parts) {
+				writer.append(part);
+			}
+			writer.newLine();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+}
