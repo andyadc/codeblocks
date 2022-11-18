c42a064c19d9755834228945c5f6a0beea3a87f1
andaicheng <andaicheng@gmail.com>
2022-11-18 16:38:46 +0800

    util

--- codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/collection/CollectionUtil.java
+++ codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/collection/CollectionUtil.java
@@ -1,6 +1,9 @@
 package com.andyadc.codeblocks.kit.collection;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public final class CollectionUtil {
 
@@ -8,6 +11,26 @@ public final class CollectionUtil {
 	 * <code>CollectionUtil</code> should not normally be instantiated.
 	 */
 	private CollectionUtil() {
+	}
+
+	/**
+	 * 交集
+	 */
+	public static Collection<?> intersect(Collection<?> coll1, Collection<?> coll2) {
+		Map<?, ?> tempMap = coll2.parallelStream().collect(
+			Collectors.toMap(Function.identity(), Function.identity(), (oldData, newData) -> newData)
+		);
+		return coll1.parallelStream().filter(tempMap::containsKey).collect(Collectors.toList());
+	}
+
+	/**
+	 * coll1中有, coll2中无的
+	 */
+	public static Collection<?> sub(Collection<?> coll1, Collection<?> coll2) {
+		Map<?, ?> tempMap = coll2.parallelStream().collect(
+			Collectors.toMap(Function.identity(), Function.identity(), (oldData, newData) -> newData)
+		);
+		return coll1.parallelStream().filter(str -> !tempMap.containsKey(str)).collect(Collectors.toList());
 	}
 
 	/**
