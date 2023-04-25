cef30b7750bf95bc4b3ed266759de89a6afbaaee
andaicheng <andaicheng@gmail.com>
2023-04-25 19:37:38 +0800

    refactor

--- bms-api/src/main/java/com/andyadc/bms/validation/PasswordConstraintValidator.java
+++ bms-api/src/main/java/com/andyadc/bms/validation/PasswordConstraintValidator.java
@@ -18,40 +18,39 @@ import java.util.Arrays;
  */
 public class PasswordConstraintValidator {
 
-	public static boolean isValid(final String password, String username) {
-		// Password Strength and Rules
-		final PasswordValidator validator = new PasswordValidator(
-			Arrays.asList(
-				// length between 6 and 18 characters
-				new LengthRule(6, 18),
-				// at least one lower-case character
-				new CharacterRule(EnglishCharacterData.LowerCase, 1),
-				// at least one upper-case character
-				new CharacterRule(EnglishCharacterData.UpperCase, 1),
-				// at least one digit character
-				new CharacterRule(EnglishCharacterData.Digit, 1),
-				// at least one symbol (special character)
-				new CharacterRule(EnglishCharacterData.Special, 1),
+	// Password Strength and Rules
+	private static final PasswordValidator validator = new PasswordValidator(
+		Arrays.asList(
+			// length between 6 and 18 characters
+			new LengthRule(6, 18),
+			// at least one lower-case character
+			new CharacterRule(EnglishCharacterData.LowerCase, 1),
+			// at least one upper-case character
+			new CharacterRule(EnglishCharacterData.UpperCase, 1),
+			// at least one digit character
+			new CharacterRule(EnglishCharacterData.Digit, 1),
+			// at least one symbol (special character)
+			new CharacterRule(EnglishCharacterData.Special, 1),
 
-				// define some illegal sequences that will fail when >= 5 chars long
-				// alphabetical is of the form 'abcde', numerical is '34567', qwery is 'asdfg'
-				// the false parameter indicates that wrapped sequences are allowed; e.g. 'xyzabc'
+			// define some illegal sequences that will fail when >= 5 chars long
+			// alphabetical is of the form 'abcde', numerical is '34567', qwery is 'asdfg'
+			// the false parameter indicates that wrapped sequences are allowed; e.g. 'xyzabc'
 //				new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 5, false),
 //				new IllegalSequenceRule(EnglishSequenceData.USQwerty, 5, false),
-				new IllegalSequenceRule(EnglishSequenceData.Numerical, 5, false),
+			new IllegalSequenceRule(EnglishSequenceData.Numerical, 5, false),
 
-				new UsernameRule(true, true),
+			new UsernameRule(true, true),
 
-				// no whitespace
-				new WhitespaceRule()
-			)
-		);
+			// no whitespace
+			new WhitespaceRule()
+		)
+	);
 
+	public static boolean isValid(final String password, String username) {
 		PasswordData passwordData = new PasswordData(password);
 		passwordData.setUsername(username);
 
 		RuleResult ruleResult = validator.validate(passwordData);
-
 		return ruleResult.isValid();
 	}
 }
