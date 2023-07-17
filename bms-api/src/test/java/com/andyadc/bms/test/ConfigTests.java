fe85b11c53b29f0f8f51c743135008f8ade1a4ed
andaicheng <andaicheng@gmail.com>
2023-07-17 09:27:44 +0800

    properties

--- bms-api/src/test/java/com/andyadc/bms/test/ConfigTests.java
+++ bms-api/src/test/java/com/andyadc/bms/test/ConfigTests.java
@@ -1,7 +1,10 @@
 package com.andyadc.bms.test;
 
+import com.andyadc.bms.config.ApiProperties;
+import com.andyadc.bms.config.PropertiesConfig;
 import com.andyadc.bms.modules.file.FileStorageSettings;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,6 +18,18 @@ public class ConfigTests {
 
 	@Value("${redis.key.prefix:}")
 	private String prefix;
+
+	@Inject
+	private PropertiesConfig propertiesConfig;
+
+	@Autowired
+	private ApiProperties apiProperties;
+
+	@Test
+	public void testProperties() {
+		System.out.println(propertiesConfig);
+		System.out.println(apiProperties);
+	}
 
 	@Test
 	public void testFileStorageSettings() {
