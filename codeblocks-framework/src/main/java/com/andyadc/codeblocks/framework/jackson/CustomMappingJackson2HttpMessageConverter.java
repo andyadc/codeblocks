0e82301830b778211c2715c3b34d686ab6a67030
andaicheng <andaicheng@gmail.com>
2023-07-27 09:05:37 +0800

    jackson

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/jackson/CustomMappingJackson2HttpMessageConverter.java
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/jackson/CustomMappingJackson2HttpMessageConverter.java
@@ -1,6 +1,7 @@
 package com.andyadc.codeblocks.framework.jackson;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -10,17 +11,21 @@ import java.text.SimpleDateFormat;
 /**
  * 自定义 Jackson2 转换
  */
-public class CustomerMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+public class CustomMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
 
 	protected static final String yyyyMMddHHmmssSSS = "yyyy-MM-dd HH:mm:ss.SSS";
 
-    public CustomerMappingJackson2HttpMessageConverter() {
+	public CustomMappingJackson2HttpMessageConverter() {
 		super();
 		ObjectMapper objectMapper = getObjectMapper();
 		// 设置null值不参与序列化(字段不被显示)
 		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		// 禁用空对象转换 json 校验
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		// BigDecimal 转换为字符串 而非数字, 避免精度丢失
+		objectMapper.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
+
 		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		objectMapper.setDateFormat(new SimpleDateFormat(yyyyMMddHHmmssSSS));
 	}
