db5b53f89e20763835b1b7c574515309e5330822
andaicheng <andaicheng@gmail.com>
2023-09-24 19:17:53 +0800

    todo

--- codeblocks-test/src/test/java/com/andyadc/codeblocks/test/Tests.java
+++ codeblocks-test/src/test/java/com/andyadc/codeblocks/test/Tests.java
@@ -11,5 +11,15 @@ public class Tests {
 
 		System.out.println(System.currentTimeMillis() - l1);
 		System.out.println((System.nanoTime() - l2) / 1000L / 1000L);
+
+		int i = 5;
+		int result = i++;
+		System.out.println(i);
+		System.out.println(result);
+
+		int j = 5;
+		int result2 = ++j;
+		System.out.println(j);
+		System.out.println(result2);
 	}
 }
