8ced376d0474be42a3c57ecb5821b4b06a9ab783
andaicheng <andaicheng@gmail.com>
2023-10-10 18:03:30 +0800

    json

--- codeblocks-spring-sample/src/test/java/com/andyadc/codeblocks/test/User.java
+++ codeblocks-spring-sample/src/test/java/com/andyadc/codeblocks/test/User.java
@@ -1,24 +1,29 @@
 package com.andyadc.codeblocks.test;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 public class User {
 
-    private Long id;
-    private String name;
-    private int num;
-    private Date birthday;
-    private List<String> hobbies;
-    private Map<String, Object> goods;
+	private Long id;
+	private String name;
+	private int num;
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	private BigDecimal amount;
+	private Date birthday;
+	private List<String> hobbies;
+	private Map<String, Object> goods;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
+	public void setId(Long id) {
+		this.id = id;
     }
 
     public String getName() {
@@ -27,27 +32,35 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
+	}
 
-    public int getNum() {
-        return num;
-    }
+	public int getNum() {
+		return num;
+	}
 
-    public void setNum(int num) {
-        this.num = num;
-    }
+	public void setNum(int num) {
+		this.num = num;
+	}
 
-    public Date getBirthday() {
-        return birthday;
-    }
+	public BigDecimal getAmount() {
+		return amount;
+	}
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
 
-    public List<String> getHobbies() {
-        return hobbies;
-    }
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public List<String> getHobbies() {
+		return hobbies;
+	}
 
     public void setHobbies(List<String> hobbies) {
         this.hobbies = hobbies;
