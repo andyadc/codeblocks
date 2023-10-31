af46b43a27a2ff6c4e176a5baee9d3a39058d022
andaicheng <andaicheng@gmail.com>
2023-10-31 17:17:17 +0800

    processor flow

--- processor-flow-template/src/main/java/com/andyadc/workflow/processor/ProcessorContext.java
+++ processor-flow-template/src/main/java/com/andyadc/workflow/processor/ProcessorContext.java
@@ -1,23 +1,48 @@
 package com.andyadc.workflow.processor;
 
-import com.andyadc.workflow.ProcessorConstant;
+import com.andyadc.workflow.base.ProcessorConstant;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProcessorContext {
 
 	/**
+	 * 内部业务请求对象
+	 */
+	public static final String TRANSACTION_FLOW_REQUEST = "transaction_flow_request";
+
+	/**
+	 * 请求对象
+	 */
+	public static final String ORDER_REQUEST = "order_request";
+
+	/**
+	 * 响应对象
+	 */
+	public static final String ORDER_RESPONSE = "order_response";
+
+	/**
+	 * 自定义的交易类型
+	 */
+	public static final String BIZ_TYPE = "biz_type";
+	private final Map<String, Object> variables = new ConcurrentHashMap<>();
+	/**
 	 * 上下文标示
 	 */
 	private String contextId;
-
 	/**
 	 * 子上下文对象的标示
 	 */
 	private String subContextId;
-
 	/**
 	 * 工作流整合类型
 	 */
 	private String integrationChannelType;
+
+	private String errorCode;
+	private String errorMessage;
 
 	/**
 	 * 当前工作状态
@@ -28,6 +53,44 @@ public class ProcessorContext {
 	 * 处理结果
 	 */
 	private String processResult = ProcessorConstant.PROCESS_CONTINUE;
+
+	public void delVariables(String key) {
+		getVariables().remove(key);
+	}
+
+	public void setVariables(String key, Object object) {
+		if (StringUtils.hasText(key) && object != null) {
+			getVariables().put(key, object);
+		}
+	}
+
+	public <T> T getVarValue(String key, Class<T> clazz) {
+		Object o = variables.get(key);
+		if (clazz.isInstance(o)) {
+			return (T) o;
+		}
+		return null;
+	}
+
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
 
 	public String getCurrentWork() {
 		return currentWork;
@@ -67,5 +130,23 @@ public class ProcessorContext {
 
 	public void setIntegrationChannelType(String integrationChannelType) {
 		this.integrationChannelType = integrationChannelType;
+	}
+
+	public String getBizType() {
+		return getVarValue(BIZ_TYPE, String.class);
+	}
+
+	public void setBizType(String bizType) {
+		setVariables(BIZ_TYPE, bizType);
+	}
+
+	@Override
+	public String toString() {
+		return "ProcessorContext{" +
+			"contextId=" + contextId +
+			", subContextId=" + subContextId +
+			", integrationChannelType=" + integrationChannelType +
+			", variables[" + !variables.isEmpty() + "]" +
+			'}';
 	}
 }
--- processor-flow-template/src/main/java/com/andyadc/workflow/processor/TxnProcessorFactory.java
+++ processor-flow-template/src/main/java/com/andyadc/workflow/processor/TxnProcessorFactory.java
@@ -1,4 +1,71 @@
 package com.andyadc.workflow.processor;
 
+import com.andyadc.workflow.processor.core.BizProcessor;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TxnProcessorFactory {
+
+	//特殊交易请求处理器集合
+	protected Map<String, BizProcessor> txnRequestProcessorMap = new HashMap<>();
+	//特殊交易返回处理器集合
+	protected Map<String, BizProcessor> txnResponseProcessorMap = new HashMap<>();
+	//特殊异常处理器集合
+	protected Map<String, BizProcessor> exceptionProcessorMap = new HashMap<>();
+
+	//默认交易处理器
+	protected BizProcessor defaultTxnRequestProcessor;
+	//默认交易返回处理器
+	protected BizProcessor defaultTxnResponseProcessor;
+	//默认异常处理器
+	protected BizProcessor defaultExceptionProcessor;
+
+	public BizProcessor getTxnRequestProcessor(ProcessorContext context) {
+		BizProcessor bizProcessor = txnRequestProcessorMap.get(context.getBizType());
+		if (bizProcessor == null) {
+			return defaultTxnRequestProcessor;
+		}
+		return bizProcessor;
+	}
+
+	public BizProcessor getTxnResponseProcessor(ProcessorContext context) {
+		BizProcessor bizProcessor = txnResponseProcessorMap.get(context.getBizType());
+		if (bizProcessor == null) {
+			return defaultTxnResponseProcessor;
+		}
+		return bizProcessor;
+	}
+
+	public BizProcessor getExceptionProcessor(ProcessorContext context) {
+		BizProcessor bizProcessor = exceptionProcessorMap.get(context.getIntegrationChannelType());
+		if (bizProcessor == null) {
+			return defaultExceptionProcessor;
+		}
+		return bizProcessor;
+	}
+
+	public void setTxnRequestProcessorMap(Map<String, BizProcessor> txnRequestProcessorMap) {
+		this.txnRequestProcessorMap = txnRequestProcessorMap;
+	}
+
+	public void setTxnResponseProcessorMap(Map<String, BizProcessor> txnResponseProcessorMap) {
+		this.txnResponseProcessorMap = txnResponseProcessorMap;
+	}
+
+	public void setExceptionProcessorMap(Map<String, BizProcessor> exceptionProcessorMap) {
+		this.exceptionProcessorMap = exceptionProcessorMap;
+	}
+
+	public void setDefaultTxnRequestProcessor(BizProcessor defaultTxnRequestProcessor) {
+		this.defaultTxnRequestProcessor = defaultTxnRequestProcessor;
+	}
+
+	public void setDefaultTxnResponseProcessor(BizProcessor defaultTxnResponseProcessor) {
+		this.defaultTxnResponseProcessor = defaultTxnResponseProcessor;
+	}
+
+	public void setDefaultExceptionProcessor(BizProcessor defaultExceptionProcessor) {
+		this.defaultExceptionProcessor = defaultExceptionProcessor;
+	}
 }
