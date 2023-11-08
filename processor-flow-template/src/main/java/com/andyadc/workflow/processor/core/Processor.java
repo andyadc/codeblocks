ede0af6077e69f326d08364fab2d6c29c277516a
andaicheng <andaicheng@gmail.com>
2023-11-08 09:00:17 +0800

    processor flow

--- processor-flow-template/src/main/java/com/andyadc/workflow/processor/core/Processor.java
+++ processor-flow-template/src/main/java/com/andyadc/workflow/processor/core/Processor.java
@@ -1,6 +1,7 @@
-package com.andyadc.workflow.processor;
+package com.andyadc.workflow.processor.core;
 
 import com.andyadc.workflow.exception.BizException;
+import com.andyadc.workflow.processor.ProcessorContext;
 
 public interface Processor {
 
