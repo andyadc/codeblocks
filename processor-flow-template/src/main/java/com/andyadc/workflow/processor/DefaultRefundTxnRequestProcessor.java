c303dc235eb4a96ee322cce74e97fcd40645eb85
andaicheng <andaicheng@gmail.com>
2023-11-13 09:24:37 +0800

    processor flow

---
+++ processor-flow-template/src/main/java/com/andyadc/workflow/processor/DefaultRefundTxnRequestProcessor.java
@@ -0,0 +1,12 @@
+package com.andyadc.workflow.processor;
+
+import com.andyadc.workflow.exception.BizException;
+import com.andyadc.workflow.processor.common.GeneralRefundTxnProcessor;
+
+public class DefaultRefundTxnRequestProcessor extends GeneralRefundTxnProcessor {
+
+	@Override
+	public void process(ProcessorContext context) throws BizException {
+		super.process(context);
+	}
+}
