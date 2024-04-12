1958076c398909e80d04f827a331c482e635cf56
andaicheng <andaicheng@gmail.com>
2024-04-12 17:48:27 +0800

    auth

---
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/auth/AuthException.java
@@ -0,0 +1,7 @@
+package com.andyadc.codeblocks.framework.auth;
+
+public class AuthException extends RuntimeException {
+
+	private static final long serialVersionUID = 7522989040697812201L;
+
+}
--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/authorize/Logical.java
+++
@@ -1,5 +0,0 @@
-package com.andyadc.codeblocks.framework.authorize;
-
-public enum Logical {
-	AND, OR
-}
