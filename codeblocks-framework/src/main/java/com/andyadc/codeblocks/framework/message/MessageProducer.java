64ea3a6a13b8a69a42caae7b218888ecdb4ce9a5
andaicheng <andaicheng@gmail.com>
2024-11-28 09:43:59 +0800

    message

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/message/MessageProducer.java
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/message/MessageProducer.java
@@ -4,5 +4,17 @@ public interface MessageProducer {
 
 	void send(String topic, Message<?> message);
 
+	/**
+	 * 向 topic 发送消息，同时提供消息的 key 和指定分区  -→  发送给指定分区
+	 *
+	 * @param topic     topic
+	 * @param partition partition
+	 * @param key       key
+	 * @param message   message
+	 */
+	void send(String topic, Integer partition, String key, Message<?> message);
+
 	void syncSend(String topic, Message<?> message);
+
+	void syncSend(String topic, Integer partition, String key, Message<?> message);
 }
