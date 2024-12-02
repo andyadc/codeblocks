3c374ea65aab567bee10b9ae49805a052de37f36
andaicheng <andaicheng@gmail.com>
2024-12-02 09:40:08 +0800

    rabbitmq

--- codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/rabbitmq/ChannelException.java
+++ codeblocks-framework/src/main/java/com/andyadc/codeblocks/framework/rabbitmq/ChannelException.java
@@ -2,6 +2,8 @@ package com.andyadc.codeblocks.framework.rabbitmq;
 
 public class ChannelException extends RuntimeException {
 
+	private static final long serialVersionUID = 9220799651524428636L;
+
 	public ChannelException(String message, Throwable cause) {
 		super(message, cause);
 	}
