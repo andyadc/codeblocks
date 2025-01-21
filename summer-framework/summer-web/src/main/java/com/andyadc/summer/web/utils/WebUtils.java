99c57efc1307d2bc1051b3bff6d41e535a644323
andaicheng <andaicheng@gmail.com>
2025-01-21 09:15:26 +0800

    summer framework | web

---
+++ summer-framework/summer-web/src/main/java/com/andyadc/summer/web/utils/WebUtils.java
@@ -0,0 +1,12 @@
+package com.andyadc.summer.web.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebUtils {
+
+	public static final String DEFAULT_PARAM_VALUE = "\0\t\0\t\0";
+
+	static final Logger logger = LoggerFactory.getLogger(WebUtils.class);
+
+}
