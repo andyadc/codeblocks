3392d1521cd4543fce8ccfc06e3cf61b8ca7508c
andaicheng <andaicheng@gmail.com>
2025-01-24 14:53:38 +0800

    summer framework | jdbc

---
+++ summer-framework/summer-jdbc/src/main/java/com/andyadc/summer/jdbc/tx/TransactionStatus.java
@@ -0,0 +1,13 @@
+package com.andyadc.summer.jdbc.tx;
+
+import java.sql.Connection;
+
+public class TransactionStatus {
+
+	final Connection connection;
+
+	public TransactionStatus(Connection connection) {
+		this.connection = connection;
+	}
+
+}
