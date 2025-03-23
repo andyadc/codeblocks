b08a8b15b4275c3828b9005be2e82ed45da6d83e
andaicheng <andaicheng@gmail.com>
2025-03-23 10:17:18 +0800

    summer framework | jdbc

--- summer-framework/summer-jdbc/src/main/java/com/andyadc/summer/jdbc/PreparedStatementCallback.java
+++ summer-framework/summer-jdbc/src/main/java/com/andyadc/summer/jdbc/PreparedStatementCallback.java
@@ -1,13 +1,10 @@
 package com.andyadc.summer.jdbc;
 
-import jakarta.annotation.Nullable;
-
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 @FunctionalInterface
 public interface PreparedStatementCallback<T> {
 
-	@Nullable
 	T doInPreparedStatement(PreparedStatement ps) throws SQLException;
 }
--- summer-framework/summer-jdbc/src/main/java/com/andyadc/summer/jdbc/tx/TransactionalUtils.java
+++ summer-framework/summer-jdbc/src/main/java/com/andyadc/summer/jdbc/tx/TransactionalUtils.java
@@ -1,12 +1,9 @@
 package com.andyadc.summer.jdbc.tx;
 
-import jakarta.annotation.Nullable;
-
 import java.sql.Connection;
 
 public class TransactionalUtils {
 
-	@Nullable
 	public static Connection getCurrentConnection() {
 		TransactionStatus ts = DataSourceTransactionManager.transactionStatus.get();
 		return ts == null ? null : ts.connection;
