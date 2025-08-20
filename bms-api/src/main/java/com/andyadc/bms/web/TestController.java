f7566e0b001234fe97f48142c44ff69bb0e61bdd
andaicheng <andaicheng@gmail.com>
2025-08-20 16:32:40 +0800

    test

--- bms-api/src/main/java/com/andyadc/bms/web/TestController.java
+++ bms-api/src/main/java/com/andyadc/bms/web/TestController.java
@@ -1,11 +1,40 @@
 package com.andyadc.bms.web;
 
+import com.andyadc.codeblocks.kit.servlet.ServletUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @RequestMapping("/test")
 @Controller
 public class TestController {
+
+	@ResponseBody
+	@RequestMapping(value = "/4",
+		consumes = "application/x-www-form-urlencoded", produces = "application/text")
+	public String test4(@RequestBody MultiValueMap<String, String> formData) {
+		Map<String, String> singleValueMap = formData.toSingleValueMap();
+		System.out.println(singleValueMap);
+		return "success";
+	}
+
+	@ResponseBody
+	@RequestMapping("/")
+	public String test(HttpServletRequest request, HttpServletResponse response) {
+		ServletUtil.printHttpServletRequest(request);
+		Map<String, String> params = ServletUtil.getReqParams(request);
+		System.out.println(params);
+
+		Map<String, String> headers = ServletUtil.getHeaders(request);
+		System.out.println(headers);
+		return "test";
+	}
 
 	@RequestMapping("/1")
 	public String test1() {
