069c3fbab4c511af59eb7c0b5827f5a67eabc1af
andaicheng <andaicheng@gmail.com>
2025-09-01 08:57:40 +0800

    jakarta

--- codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/net/IPUtil.java
+++ codeblocks-kit/src/main/java/com/andyadc/codeblocks/kit/net/IPUtil.java
@@ -1,9 +1,9 @@
 package com.andyadc.codeblocks.kit.net;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
