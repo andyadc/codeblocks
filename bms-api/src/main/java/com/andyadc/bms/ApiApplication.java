f25e6cd1677be665213b6dbfc2b5fd690191ff96
andaicheng <andaicheng@gmail.com>
2025-12-10 15:15:55 +0800

    spring boot

--- bms-api/src/main/java/com/andyadc/bms/ApiApplication.java
+++ bms-api/src/main/java/com/andyadc/bms/ApiApplication.java
@@ -2,8 +2,15 @@ package com.andyadc.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = {
+		"com.andyadc"
+	},
+	exclude = {
+		MongoAutoConfiguration.class
+	})
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
