cd72bcba1ebdfb2168967c46f738f3b53a134c3d
andaicheng <andaicheng@gmail.com>
2026-03-14 13:19:37 +0800

    spring boot

---
+++ bms-api/src/main/java/com/andyadc/bms/AppListener.java
@@ -0,0 +1,20 @@
+package com.andyadc.bms;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.CommandLineRunner;
+
+public class AppListener implements CommandLineRunner, DisposableBean {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("应用启动成功，预加载相关数据");
+
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("应用正在关闭，清理相关数据");
+
+	}
+
+}
